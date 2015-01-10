21f194df98ed037ae4a786a4708c2cac15e9f570
Daniel Vinntreus <gudski@gmail.com>
2015-01-10 16:39:23 +0100

    add keydown event listeners

--- keyboard.js
+++ keyboard.js
@@ -1,13 +1,32 @@
 var keyCodes = {
   LEFT : 37,
   RIGHT : 39,
-  SPACE : 32
+  SPACE : 32,
+  P : 80,
+  R : 82
 };
 var keyDown = {
   LEFT : false,
   RIGHT : false,
   SPACE : false
 };
+var keyboard = (function(){
+  var listeners = [];
+  
+  document.addEventListener('keydown', function(e){
+    listeners.forEach(function(listener){
+      if(e.which === listener.keycode){
+        listener.fn();
+      }
+    });
+  });
+
+  return {
+    on : function(keycode, fn){
+      listeners.push({keycode : keycode, fn : fn});
+    }
+  };
+}());
 window.onkeydown = function(e){
   if(e.keyCode === keyCodes.LEFT) { keyDown.LEFT = true; }
   if(e.keyCode === keyCodes.RIGHT){ keyDown.RIGHT = true; }
